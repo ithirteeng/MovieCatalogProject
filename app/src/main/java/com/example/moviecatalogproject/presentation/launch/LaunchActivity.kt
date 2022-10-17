5cae4f6e780a908edbe242ffad5fd7a567e8a9fd
Gulevskii Ivan <ithirteengw@gmail.com>
2022-10-17 19:37:41 +0700

    1) Edited launch screen 2) Added dimens.xml values

---
+++ app/src/main/java/com/example/moviecatalogproject/presentation/launch/LaunchActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moviecatalogproject.presentation.launch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviecatalogproject.R
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+}
