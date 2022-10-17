26b3a88b6c9a954c66ff959098a7fc257f6ca39d
Gulevskii Ivan <ithirteengw@gmail.com>
2022-10-17 21:13:43 +0700

    renamed file..

--- app/src/main/java/com/example/moviecatalogproject/presentation/sign_up/SignUpActivity.kt
+++ app/src/main/java/com/example/moviecatalogproject/presentation/sign_up/SignUpActivity.kt
@@ -1,12 +1,12 @@
-package com.example.moviecatalogproject.presentation.sign_in
+package com.example.moviecatalogproject.presentation.sign_up
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.moviecatalogproject.R
 
-class SignInActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        setContentView(R.layout.activity_sign_up)
     }
 }
