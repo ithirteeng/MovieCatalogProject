7b5193f98f60bf3b3e6c5f38096b7461beef39c1
Gulevskii Ivan <ithirteengw@gmail.com>
2022-10-17 20:24:10 +0700

    1) created style for edit texts 2) made up the half of design

---
+++ app/src/main/java/com/example/moviecatalogproject/presentation/sign_in/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moviecatalogproject.presentation.sign_in
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviecatalogproject.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
