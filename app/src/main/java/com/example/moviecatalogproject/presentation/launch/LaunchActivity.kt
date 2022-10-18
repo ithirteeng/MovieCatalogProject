6be2df55d722c6af7d9074d70c5be3d6f9f2d031
Gulevskii Ivan <ithirteengw@gmail.com>
2022-10-18 21:17:35 +0700

    1) added intents between screens

--- app/src/main/java/com/example/moviecatalogproject/presentation/launch/LaunchActivity.kt
+++ app/src/main/java/com/example/moviecatalogproject/presentation/launch/LaunchActivity.kt
@@ -1,12 +1,30 @@
 package com.example.moviecatalogproject.presentation.launch
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.moviecatalogproject.R
+import com.example.moviecatalogproject.presentation.sign_up.SignUpActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1500)
+            makeIntent()
+
+        }
+
+    }
+
+    private fun makeIntent() {
+        startActivity(Intent(this, SignUpActivity::class.java))
+        finish()
     }
 }
--- app/src/main/java/com/example/moviecatalogproject/presentation/sign_up/SignUpActivity.kt
+++ app/src/main/java/com/example/moviecatalogproject/presentation/sign_up/SignUpActivity.kt
@@ -1,8 +1,10 @@
 package com.example.moviecatalogproject.presentation.sign_up
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.moviecatalogproject.databinding.ActivitySignUpBinding
+import com.example.moviecatalogproject.presentation.sign_in.SignInActivity
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -13,5 +15,19 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setupButtonOnClickFunctions()
+    }
+
+    private fun setupButtonOnClickFunctions() {
+        onRegistrationButtonClick()
+    }
+
+    private fun onRegistrationButtonClick() {
+        binding.registrationButton.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+            overridePendingTransition(0,0)
+            finish()
+        }
     }
 }
