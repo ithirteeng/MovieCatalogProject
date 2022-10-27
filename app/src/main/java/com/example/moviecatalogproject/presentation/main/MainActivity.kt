0d6eda606d7a757a648a1f9d926e09212d39ccac
Gulevskii Ivan <ithirteengw@gmail.com>
2022-10-27 15:58:51 +0700

    1) made up rabLayout with recoloring of selected tabs 2) created two fragments for MainActivity.kt 3) implemented the logic of replacing fragments on tab select

--- app/src/main/java/com/example/moviecatalogproject/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/moviecatalogproject/presentation/main/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.moviecatalogproject.presentation.main
 
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.example.moviecatalogproject.R
 import com.example.moviecatalogproject.databinding.ActivityMainBinding
+import com.example.moviecatalogproject.presentation.main.movie.MovieFragment
+import com.example.moviecatalogproject.presentation.main.profile.ProfileFragment
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import com.google.android.material.tabs.TabLayout.Tab
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +21,52 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        replaceToMovieFragment()
+        onTabsSelected()
+    }
+
+    private fun onTabsSelected() {
+        colorizeTab(
+            binding.tabLayout.getTabAt(0)!!,
+            resources.getColor(R.color.accent, theme)
+        )
+        binding.tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: Tab) {
+                when (tab.position) {
+                    0 -> replaceToMovieFragment()
+                    1 -> replaceToProfileFragment()
+                }
+                colorizeTab(tab, resources.getColor(R.color.accent, theme))
+            }
+
+            override fun onTabUnselected(tab: Tab) {
+                colorizeTab(tab, resources.getColor(R.color.gray, theme))
+            }
+
+            override fun onTabReselected(tab: Tab?) {
+            }
+
+        })
+    }
+
+    private fun colorizeTab(tab: Tab, color: Int) {
+        tab.icon?.colorFilter = PorterDuffColorFilter(color, PorterDuff.Mode.SRC_IN)
+    }
+
+    private fun replaceToMovieFragment() {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(R.id.fragmentContainerView, MovieFragment())
+
+        }
+    }
+
+    private fun replaceToProfileFragment() {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(R.id.fragmentContainerView, ProfileFragment())
+
+        }
     }
 }
