ef07565a910750e5a1e12358975e3b33fd1a0980
Gulevskii Ivan <ithirteengw@gmail.com>
2022-11-03 21:41:30 +0700

    1) understood coordinator layout 2) made a mock for favourites button in activity

--- app/src/main/java/com/example/moviecatalogproject/presentation/movie_info/MovieInfoActivity.kt
+++ app/src/main/java/com/example/moviecatalogproject/presentation/movie_info/MovieInfoActivity.kt
@@ -1,6 +1,7 @@
 package com.example.moviecatalogproject.presentation.movie_info
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.moviecatalogproject.databinding.ActivityMovieInfoBinding
 
@@ -17,7 +18,31 @@ class MovieInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+            setDisplayShowTitleEnabled(false)
+        }
 
+        binding.button.visibility = View.GONE
+
+        binding.toolbarLayout.title = ":3"
+
+        binding.appbar.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+            if (kotlin.math.abs(verticalOffset) - appBarLayout.totalScrollRange == 0) {
+                if (binding.button.visibility == View.GONE) {
+                    binding.button.visibility = View.VISIBLE
+                }
+            } else {
+                if (binding.button.visibility == View.VISIBLE) {
+                    binding.button.visibility = View.GONE
+                }
+            }
+        }
+
+
+        binding.toolbar.setNavigationOnClickListener { finish() }
 
     }
 }
